dece87315f30e3600f408ebe920a24c217b59873
Vrajesh Nandha <vrajeshnandha97@gmail.com>
2020-04-06 15:27:51 +0530

    adding error message

--- homesite/views.py
+++ homesite/views.py
@@ -8,31 +8,36 @@ def home(request):
 
 
 def addStock(request):
-    level = request.POST.get('level')
-    fabric_type = request.POST.get('fabric_type')
-    glove_type = request.POST.get('glove_type')
-    name = request.POST.get('name')
-    quantity = request.POST.get('quantity')
     fabric = Fabric.objects.all()
     cutting = Cutting.objects.all()
     context = {'fabric': fabric, 'cutting': cutting}
+    
+    if request.method == 'GET':
+        return render(request, 'homesite/index.html', context)
 
-    if level == "None":
-        error = "Please select a level"
-        context = {'error': error}
-        return render(request, 'homesite/error.html', context)
+    else:
+        level = request.POST.get('level')
+        fabric_type = request.POST.get('fabric_type')
+        glove_type = request.POST.get('glove_type')
+        name = request.POST.get('name')
+        quantity = request.POST.get('quantity')
 
-    if level == "Fabric":
-        fabric_computation(fabric_type, quantity)
-
-    if level == "Cutting":
-        flag = cutting_computation(fabric_type, glove_type, quantity, name)
-        if flag == 0:
-            error = "Fabric is not sufficient for cutting"
+        if level is None:
+            error = "Please select a level"
             context = {'error': error}
             return render(request, 'homesite/error.html', context)
 
-    return render(request, 'homesite/index.html', context)
+        if level == "Fabric":
+            fabric_computation(fabric_type, quantity)
+
+        if level == "Cutting":
+            flag = cutting_computation(fabric_type, glove_type, quantity, name)
+            if flag == 0:
+                error = "Fabric is not sufficient for cutting"
+                context = {'error': error}
+                return render(request, 'homesite/error.html', context)
+
+        return render(request, 'homesite/index.html', context)
 
 
 def viewStock(request):
